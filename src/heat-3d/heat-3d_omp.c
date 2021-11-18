d7ca79f42c4c92d7b6e0dd60f6eb9b6885d1a0ff
Luke Volpatti <luke.volpatti@gmail.com>
2021-11-18 22:05:46 +0100

    heat-3d omp working, gives some speedup.

--- src/heat-3d/heat-3d_omp.c
+++ src/heat-3d/heat-3d_omp.c
@@ -67,13 +67,13 @@ void kernel_heat_3d(int tsteps,
 		      DATA_TYPE POLYBENCH_3D(A,N,N,N,n,n,n),
 		      DATA_TYPE POLYBENCH_3D(B,N,N,N,n,n,n))
 {
-  int t, i, j, k;
 
 #pragma scop
-    for (t = 1; t <= TSTEPS; t++) {
-        for (i = 1; i < _PB_N-1; i++) {
-            for (j = 1; j < _PB_N-1; j++) {
-                for (k = 1; k < _PB_N-1; k++) {
+    for (int t = 1; t <= TSTEPS; t++) {
+        #pragma omp parallel for
+        for (int i = 1; i < _PB_N-1; i++) {
+            for (int j = 1; j < _PB_N-1; j++) {
+                for (int k = 1; k < _PB_N-1; k++) {
                     B[i][j][k] =   SCALAR_VAL(0.125) * (A[i+1][j][k] - SCALAR_VAL(2.0) * A[i][j][k] + A[i-1][j][k])
                                  + SCALAR_VAL(0.125) * (A[i][j+1][k] - SCALAR_VAL(2.0) * A[i][j][k] + A[i][j-1][k])
                                  + SCALAR_VAL(0.125) * (A[i][j][k+1] - SCALAR_VAL(2.0) * A[i][j][k] + A[i][j][k-1])
@@ -81,9 +81,9 @@ void kernel_heat_3d(int tsteps,
                 }
             }
         }
-        for (i = 1; i < _PB_N-1; i++) {
-           for (j = 1; j < _PB_N-1; j++) {
-               for (k = 1; k < _PB_N-1; k++) {
+        for (int i = 1; i < _PB_N-1; i++) {
+           for (int j = 1; j < _PB_N-1; j++) {
+               for (int k = 1; k < _PB_N-1; k++) {
                    A[i][j][k] =   SCALAR_VAL(0.125) * (B[i+1][j][k] - SCALAR_VAL(2.0) * B[i][j][k] + B[i-1][j][k])
                                 + SCALAR_VAL(0.125) * (B[i][j+1][k] - SCALAR_VAL(2.0) * B[i][j][k] + B[i][j-1][k])
                                 + SCALAR_VAL(0.125) * (B[i][j][k+1] - SCALAR_VAL(2.0) * B[i][j][k] + B[i][j][k-1])
