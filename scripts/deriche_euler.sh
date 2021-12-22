0cf3ca4aca9771f7fc3e424d2a5a6f2c4e2ee259
Kaishuo Zhang <6542131+zhangks98@users.noreply.github.com>
2021-12-22 14:46:45 +0100

    Reduced benchmark size.

--- scripts/deriche_euler.sh
+++ scripts/deriche_euler.sh
@@ -5,11 +5,9 @@ SCRIPTS_DIR=$(dirname "$0")
 cd $SCRIPTS_DIR
 pwd
 
-bsub -R "model=EPYC_7H12 rusage[mem=4000]" -W 24:00 -J "deriche" -n 32 <<EOF
-./benchmark_deriche.sh 50 deriche_mpi_baseline
-./benchmark_deriche.sh 50 deriche_mpi_rdma
-./benchmark_deriche.sh 50 deriche_mpi_segments
-./benchmark_deriche.sh 50 deriche_mpi_avx
-./benchmark_deriche.sh 50 deriche_omp
-./benchmark_deriche.sh 50 deriche_ref
-EOF
+# bsub -R "model=EPYC_7H12 rusage[mem=4000]" -W 4:00 -J "deriche_mpi_baseline" -n 32 ./benchmark_deriche.sh 25 deriche_mpi_baseline
+# bsub -R "model=EPYC_7H12 rusage[mem=4000]" -W 4:00 -J "deriche_mpi_rdma" -n 32 ./benchmark_deriche.sh 25 deriche_mpi_rdma
+bsub -R "model=EPYC_7H12 rusage[mem=4000]" -W 4:00 -J "deriche_mpi_segments" -n 32 ./benchmark_deriche.sh 25 deriche_mpi_segments
+bsub -R "model=EPYC_7H12 rusage[mem=4000]" -W 4:00 -J "deriche_mpi_avx" -n 32 ./benchmark_deriche.sh 25 deriche_mpi_avx
+bsub -R "model=EPYC_7H12 rusage[mem=4000]" -W 4:00 -J "deriche_omp" -n 32 ./benchmark_deriche.sh 25 deriche_omp
+bsub -R "model=EPYC_7H12 rusage[mem=4000]" -W 4:00 -J "deriche_ref" -n 1 ./benchmark_deriche.sh 25 deriche_ref
